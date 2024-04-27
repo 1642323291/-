e9ba38db409b26a988cedd783d98fd367599b4c2
1642323291 <164126871+1642323291@users.noreply.github.com>
2024-04-27 18:04:08 +0800

    Add files via upload

---
+++ kecheng c/栈.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+#define MaxSize 10000
+typedef int ElemenType;
+typedef struct SNode{ 
+    ElemenType data[MaxSize];
+    int top;
+};
